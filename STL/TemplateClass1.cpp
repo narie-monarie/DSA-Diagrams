0461686c964651dcd97bf920f00b0ecda1c4ed48
Monari <monarijob@yahoo.com>
2022-09-02 16:43:31 +0300

    stl

---
+++ STL/TemplateClass1.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+#define loop(a, b, c) for (int(a) = (b); (a) < (c); ++(a))
+typedef long long LL;
+
+template <typename R, typename T, typename U>
+R max(T a, U b) {
+  return b > a ? b : a;
+}
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  int i = 0, b = 7;
+  long z = 2, n = 8;
+  auto d = max<double>(5.5, 2);
+  cout << d;
+}
+
